e336b7cf05940f5152667820aa639013109e2807
Rodrigo Cavalcanti <rodrigo.cavalcanti@liferay.com>
2026-01-16 00:44:28 -0300

    002 - Use .env

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://vxrfixckdourckrkporx.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4cmZpeGNrZG91cmNrcmtwb3J4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0NDUwMTcsImV4cCI6MjA4NDAyMTAxN30.qzpsE_dqddds8Gyisb8QoAxsIySXM4lNfpUpSeE7t54   ';
+// @ts-ignore
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+// @ts-ignore
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
